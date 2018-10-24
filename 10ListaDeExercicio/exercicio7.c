88b0256ce495d3d8e22bc6c9c261f2c8a5ddc9d7
gabrielfeba <gabrielfeba@outlook.com>
2018-10-24 12:18:39 -0300

    reparar erro de intepretação no exercício.

--- 10ListaDeExercicio/exercicio7.c
+++ 10ListaDeExercicio/exercicio7.c
@@ -4,12 +4,11 @@
 int main(int argc, char* argv[]) {
     int numeroFatorial, resultado;
     resultado = 1;
-    printf("Qual o numero fatorial ? ");
-    scanf("%d", &numeroFatorial);
+    numeroFatorial = 6;
     for(int contador = 1; contador <= numeroFatorial; contador++) {
         resultado = resultado * contador;
     }
-    printf("\nO resultado eh : %d", resultado);
+    printf("\nO resultado do fatorial de 6 eh : %d", resultado);
     system("PAUSE");
     return 0;
 }
