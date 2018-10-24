38144708e0e4dea3b755a486b69b4ac122ce4e22
gabrielfeba <gabrielfeba@outlook.com>
2018-10-24 12:57:45 -0300

    adicionar décimo quarto exercício da décima lista.

---
+++ 10ListaDeExercicio/exercicio14.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    int cont;
+    for(cont = 10; cont >= 1; cont--) {
+        printf("%d\n", cont);
+    }
+    system("PAUSE");
+    return 0;
+}
